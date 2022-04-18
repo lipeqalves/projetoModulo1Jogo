cf1fa5afbbaf50d49118f2c7ca2032e6f06c0f52
Filipe Alves <filipe@pop-os.localdomain>
2022-04-18 16:25:33 -0300

    começa o jogo para o personagem um, adiciona novas paginas, e faz a logica do codigo do jogo no javascript

--- script/char01.js
+++ script/char01.js
@@ -1,28 +1,69 @@
 //ZEUS
-const escolhaZeus = document.querySelector("#zeusEscolha");
-escolhaZeus.addEventListener('click', personagemZeus);
-
-function personagemZeus(){
+function personagemZeusetapa1(){
     var cont = 0;  
     var chance = 2; 
     while(cont < 2){
-        var presente = prompt(`Zeus esta tentando se vingar, e envia um presente para o epimeteu, Qual o presente?
-       
-            a - Carro
-            b - Casa
-            c - Uma bela mulher com uma caixa
-           `);
-    if(presente == "c"){
-        alert(`Presente enviado!!!`);
-        break;
-    }else if(presente == "a" || presente == "b"){
-        chance --;
-        alert(`Opção Errada!!! Você tem ${chance} chances`);
-        cont++;
-    }else{
-        chance --
-        alert(`Opção Invalida!!! Você tem ${chance} chances`);
-        cont++;
+        var presente = prompt('Digite sua resposta: ')
+        if(presente == "c"){
+            alert(`Você ta sabendo da Historia!!!`);
+            window.location.assign("./char1etapa2.html");
+            break;
+        }else if(presente == "a" || presente == "b" || presente == "d"){
+            chance --;
+            alert(`Opção Errada!!! Você tem ${chance} chances`);
+            window.location.assign("./escolhePersonagem.html");
+            cont++;
+        }else{
+            chance --
+            alert(`Opção Invalida!!! Você tem ${chance} chances`);
+            window.location.assign("./escolhePersonagem.html");
+            cont++;
+        }
     }
 }
+
+function personagemZeusetapa2(){
+    var cont = 0;  
+    var chance = 2; 
+    while(cont < 2){
+        var presente = prompt('Digite sua resposta: ')
+        if(presente == "b"){
+            alert(`Parabens!! Acertou mais uma`);
+            window.location.assign("./char1etapa3.html");
+            break;
+        }else if(presente == "a" || presente == "d" || presente == "c"){
+            chance --;
+            alert(`Opção Errada!!! Você tem ${chance} chances`);
+            window.location.assign("./escolhePersonagem.html");
+            cont++;
+        }else{
+            chance --
+            alert(`Opção Invalida!!! Você tem ${chance} chances`);
+            window.location.assign("./escolhePersonagem.html");
+            cont++;
+        }
+    }
+}
+
+function personagemZeusetapa3(){
+    var cont = 0;  
+    var chance = 2; 
+    while(cont < 2){
+        var presente = prompt('Digite sua resposta: ')
+        if(presente == "d"){
+            alert(`Presente Enviado!!!`);
+            window.location.assign("./char1final.html");
+            break;
+        }else if(presente == "a" || presente == "b" || presente == "c"){
+            chance --;
+            alert(`Opção Errada!!! Você tem ${chance} chances`);
+            window.location.assign("./escolhePersonagem.html");
+            cont++;
+        }else{
+            chance --
+            alert(`Opção Invalida!!! Você tem ${chance} chances`);
+            window.location.assign("./escolhePersonagem.html");
+            cont++;
+        }
+    }
 }
