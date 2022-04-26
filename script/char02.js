ad8eafd13da13658cdc7f54e3d24fc8c3a91adcc
Filipe <lipeqa@gmail>
2022-04-26 01:17:06 -0300

    Cria historia do segundo personagem, coloca link na ultima pagina e coloca movimento na imagem de escolha de personagem

--- script/char02.js
+++ script/char02.js
@@ -4,11 +4,11 @@ function personagemPandoraEtapa1(){
     var chance = 2; 
     while(cont < 2){
         var presente = prompt('Digite sua resposta para continuar ou digite sair para finalizar o jogo ')
-        if(presente == "c"){
+        if(presente == "d"){
             alert(`Você ta sabendo da Historia!!!`);
             window.location.assign("../char2_html/char2etapa2.html");
             break;
-        }else if(presente == "a" || presente == "b" || presente == "d"){
+        }else if(presente == "a" || presente == "b" || presente == "d" || presente == "e"){
             chance --;
             alert(`Opção Errada!!! Você tem ${chance} chances`);
             window.location.assign("../finalDoJogo.html");
@@ -22,13 +22,13 @@ function personagemPandoraEtapa1(){
             alert(`Opção Errada!!! Você tem ${chance} chances`);
             window.location.assign("../finalDoJogo.html");
             cont++;
-        }
+        }/*
         else{
             chance --;
             alert(`Opção Errada!!! Você tem ${chance} chances`);
             window.location.assign("../finalDoJogo.html");
             cont++;
-        }
+        }*/
     }
 }
 function personagemPandoraEtapa2(){
@@ -36,11 +36,11 @@ function personagemPandoraEtapa2(){
     var chance = 2; 
     while(cont < 2){
         var presente = prompt('Digite sua resposta para continuar ou digite sair para finalizar o jogo ')
-        if(presente == "b"){
+        if(presente == "e"){
             alert(`Parabens!! Acertou mais uma`);
             window.location.assign("../char2_html/char2etapa3.html");
             break;
-        }else if(presente == "a" || presente == "d" || presente == "c"){
+        }else if(presente.toUpperCase == "a" || presente.toUpperCase == "b" || presente.toUpperCase == "d" || presente.toUpperCase == "c"){
             chance --;
             alert(`Opção Errada!!! Você tem ${chance} chances`);
             window.location.assign("../finalDoJogo.html");
@@ -54,13 +54,13 @@ function personagemPandoraEtapa2(){
             alert(`Opção Errada!!! Você tem ${chance} chances`);
             window.location.assign("../finalDoJogo.html");
             cont++;
-        }
+        }/*
         else{
             chance --;
             alert(`Opção Errada!!! Você tem ${chance} chances`);
             window.location.assign("../finalDoJogo.html");
             cont++;
-        }
+        }*/
     }
 }
 function personagemPandoraEtapa3(){
@@ -68,11 +68,11 @@ function personagemPandoraEtapa3(){
     var chance = 2; 
     while(cont < 2){
         var presente = prompt('Digite sua resposta para continuar ou digite sair para finalizar o jogo ')
-        if(presente == "d"){
+        if(presente == "a"){
             alert(`Presente Enviado!!!`);
             window.location.assign("../char2_html/char2final.html");
             break;
-        }else if(presente == "a" || presente == "b" || presente == "c"){
+        }else if(presente.toUpperCase == "e" || presente.toUpperCase == "b" || presente.toUpperCase == "d" || presente.toUpperCase == "c"){
             chance --;
             alert(`Opção Errada!!! Você tem ${chance} chances`);
             window.location.assign("../finalDoJogo.html");
@@ -86,12 +86,12 @@ function personagemPandoraEtapa3(){
             alert(`Opção Errada!!! Você tem ${chance} chances`);
             window.location.assign("../finalDoJogo.html");
             cont++;
-        }
+        }/*
         else{
             chance --;
             alert(`Opção Errada!!! Você tem ${chance} chances`);
             window.location.assign("../finalDoJogo.html");
             cont++;
-        }
+        }*/
     }
 }
